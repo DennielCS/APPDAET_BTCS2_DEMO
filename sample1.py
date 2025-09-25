00fd0e41e66b73a44dd686e578584585c2656e90
Dugenia <12413495@benilde.edu.ph>
2025-09-25 14:29:23 +0800

    Added gender input

--- sample1.py
+++ sample1.py
@@ -5,6 +5,7 @@ name = input("What is your name?")
 age = input("How old are you?")
 color = input("What is your favourite color?")
 university = input("What is your university?")
+gender = input("What is your gender?")
 
 print(f"Hello {name}! You are {age} years old, and your favorite color is {color}. I'm currently studying at {university}.")
 
